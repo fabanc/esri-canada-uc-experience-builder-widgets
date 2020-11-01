d1c98d7eaf7891816476c1256c28065ea945faf6
fabanc <fabien.ancelin@gmail.com>
2020-11-01 18:03:50 -0500

    Code cleanup

--- working-with-feature-layers/step-4-plugging-triggers/feature-layer-count/src/runtime/widget.tsx
+++ working-with-feature-layers/step-4-plugging-triggers/feature-layer-count/src/runtime/widget.tsx
@@ -4,11 +4,7 @@ import {IMState, BaseWidget, classNames, FormattedMessage, defaultMessages as ji
 import {AllWidgetProps, css, jsx, styled} from 'jimu-core';
 import {DataRecord, DataSource, DataSourceTypes, FeatureQueryDataSource} from 'jimu-core';
 import {IMConfig} from '../config';
-import {ArcGISDataSourceTypes} from 'jimu-arcgis';
 import {FeatureLayerDataSource, FeatureLayerViewDataSource } from 'jimu-arcgis/arcgis-data-source';
-import Query = require('esri/tasks/support/Query');
-
-import { TabContent, TabPane, Nav, NavItem, NavLink, Button} from 'jimu-ui';
 import defaultMessages from './translations/default';
 
 interface Props{
@@ -17,20 +13,15 @@ interface Props{
   appMode: AppMode,
   draggingWidget: any,
   browserSizeMode: BrowserSizeMode,
-  currentPageId: string,
+  currentPageId: string
 }
 
 interface States {
-  datasource: FeatureLayerDataSource | FeatureQueryDataSource | FeatureLayerViewDataSource;
+  datasource: FeatureLayerDataSource | FeatureQueryDataSource | FeatureLayerViewDataSource
 }
-
-//function isDsConfigured(props: AllWidgetProps<IMConfig>): boolean{
-//  return props.useDataSources && !!props.useDataSources[0];
-//}
 
 export default class Widget extends BaseWidget<AllWidgetProps<IMConfig> & Props, States>{
 
-    
   isDsConfigured = () => {
     if (this.props.useDataSources &&
       this.props.useDataSources.length === 1) {
@@ -43,16 +34,14 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig> & Props,
     if (!this.isDsConfigured()) {
       return;
     }
-	
-	let where = '1=1';
-	let outFields =  ['*'];
-	let geometry = (this.props.stateProps && this.props.stateProps.EXTENT_CHANGE) ? this.props.stateProps.EXTENT_CHANGE: null
-	
-	return {
-		where: '1=1',
-		outFields: outFields,
-		geometry: geometry
-	}
+
+    let geometry = (this.props.stateProps && this.props.stateProps.EXTENT_CHANGE) ? this.props.stateProps.EXTENT_CHANGE: null;
+
+    return {
+      where: '1=1',
+      outFields: ['*'],
+      geometry: geometry
+    }
 
   }
 
@@ -60,23 +49,19 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig> & Props,
     super(props);
 
     let stateObj: States = {
-		datasource: undefined
+		  datasource: undefined
     };
 
     this.state = stateObj;
   }
 
   render(){
-	  
+
     const {useDataSources, stateProps} = this.props;
     const {datasource} = this.state;
-	console.log("State: ", this.state);
-	let query = this.query();
-	console.log('Query: ', query);
-	
+	  let query = this.query();
     return <div className="widget-demo jimu-widget" style={{overflow: 'auto'}}>
       {
-        
         <DataSourceComponent query={query} widgetId={this.props.id} useDataSource={useDataSources[0]} onDataSourceCreated={this.onDs}>
         {this.renderCount.bind(this)}
         </DataSourceComponent>
@@ -88,8 +73,7 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig> & Props,
   renderCount (ds: DataSource, queryStatus: DataSourceStatus, records:DataRecord[], toto:any) {
     let featureCount = 0;
     if(this.isDsConfigured()){
-		featureCount = ds.getRecords().length;
-		console.log('Records: ', ds.records, ' - Count: ', ds.count, 'Records Length: ', ds.getRecords().length); 
+		  featureCount = ds.getRecords().length;
     }
     return <span>{defaultMessages.featuresDisplayed} : {featureCount}</span>
   }
